c6e1e38322106501488a59bfad62aa1cf4855389
kocchan <59197715+kocchan@users.noreply.github.com>
2020-01-07 17:02:34 +0900

    created message-model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   #アソシエーション
+  has_many :messages
   has_many :group_users
   has_many :users, through: :group_users
   #nameカラムにバリデーションをかける（空でない、一意性）
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   #アソシエーション
+  has_many :messages
   has_many :groups, through: :group_users
   has_many :group_users
 end
